b2c1796f53f71d54895792aca7a74fdb22f9478c
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-04-15 01:37:12 +0700

    Fixing modal

---
+++ resources/views/components/collapse.blade.php
@@ -0,0 +1,26 @@
+<div data-toggle="collapse" data-target="#collapseList" aria-expanded="false" aria-controls="collapseList"
+    class="flex items-center justify-between min-w-60 cursor-pointer py-1.5 px-2.5 rounded-md align-middle transition-all duration-300 ease-in text-slate-600 hover:text-slate-800 dark:text-slate-300 dark:hover:text-white hover:bg-slate-200 dark:hover:bg-slate-700 focus:bg-slate-200 dark:focus:bg-slate-700 focus:text-slate-800 dark:focus:text-white">
+    {{ $label }}
+    <span data-icon class="grid place-items-center shrink-0">
+        <svg width="1.5em" height="1.5em" stroke-width="1.5" viewBox="0 0 24 24" fill="none"
+            xmlns="http://www.w3.org/2000/svg" color="currentColor" class="h-5 w-5 stroke-[1.5] dark:stroke-slate-300">
+            <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
+        </svg>
+    </span>
+</div>
+<div class="overflow-hidden transition-[max-height] duration-300 ease-in-out max-h-0" id="collapseList">
+    <ul class="flex flex-col gap-0.5 min-w-60">
+        <li
+            class="flex items-center cursor-pointer py-1.5 px-2.5 rounded-md align-middle select-none font-sans transition-all duration-300 ease-in bg-transparent text-slate-600 hover:text-slate-800 dark:text-slate-300 dark:hover:text-white hover:bg-slate-200 dark:hover:bg-slate-700 focus:bg-slate-200 dark:focus:bg-slate-700 focus:text-slate-800 dark:focus:text-white">
+            Inbox
+        </li>
+        <li
+            class="flex items-center cursor-pointer py-1.5 px-2.5 rounded-md align-middle select-none font-sans transition-all duration-300 ease-in bg-transparent text-slate-600 hover:text-slate-800 dark:text-slate-300 dark:hover:text-white hover:bg-slate-200 dark:hover:bg-slate-700 focus:bg-slate-200 dark:focus:bg-slate-700 focus:text-slate-800 dark:focus:text-white">
+            Trash
+        </li>
+        <li
+            class="flex items-center cursor-pointer py-1.5 px-2.5 rounded-md align-middle select-none font-sans transition-all duration-300 ease-in bg-transparent text-slate-600 hover:text-slate-800 dark:text-slate-300 dark:hover:text-white hover:bg-slate-200 dark:hover:bg-slate-700 focus:bg-slate-200 dark:focus:bg-slate-700 focus:text-slate-800 dark:focus:text-white">
+            Settings
+        </li>
+    </ul>
+</div>
--- resources/views/components/modal-tambah-surat.blade.php
+++ resources/views/components/modal-tambah-surat.blade.php
@@ -8,9 +8,11 @@
 
         <div class="bg-white rounded-lg w-10/12 lg:w-8/12 transition-transform duration-300 ease-out scale-100">
             <div class="pt-4 px-4 flex justify-between items-start">
-                <div class="flex flex-col">
-                    <h1 class="text-xl text-slate-800 font-bold">Sign In</h1>
-                    <p class="text-slate-600 ">Enter your email and password to Sign In.</p>
+                <div class="flex flex-col gap-2 w-full">
+                    <div class="flex flex-col">
+                        <h1 class="text-xl text-gray-600 font-bold">Entry Surat Masuk</h1>
+                    </div>
+                    <hr class="w-full border-t border-gray-300" />
                 </div>
                 <button type="button" data-dismiss="modal" aria-label="Close"
                     class="inline-grid place-items-center border align-middle select-none font-sans font-medium text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none data-[shape=circular]:rounded-full text-sm min-w-[34px] min-h-[34px] rounded-full bg-transparent border-transparent text-slate-200-foreground hover:bg-slate-200/10 hover:border-slate-200/10 shadow-none hover:shadow-none outline-none absolute right-2 top-2">
@@ -23,44 +25,55 @@
                 </button>
             </div>
             <div class="p-4">
-                <div class="mb-4 mt-2 space-y-1.5">
-                    <label for="email"
-                        class="font-sans  text-sm text-slate-800 dark:text-white font-bold mb-2">Email</label>
-                    <div class="relative w-full">
-                        <input placeholder="someone@example.com" type="email"
-                            class="w-full aria-disabled:cursor-not-allowed outline-none focus:outline-none text-slate-800 dark:text-white placeholder:text-slate-600/60 bg-transparent ring-transparent border border-slate-200 transition-all duration-300 ease-in disabled:opacity-50 disabled:pointer-events-none data-[error=true]:border-error data-[success=true]:border-success select-none data-[shape=pill]:rounded-full text-sm rounded-md py-2 px-2.5 ring shadow-sm data-[icon-placement=start]:ps-9 data-[icon-placement=end]:pe-9 hover:border-slate-800 hover:ring-slate-800/10 focus:border-slate-800 focus:ring-slate-800/10 peer" />
+                <div class="flex flex-row gap-3">
+                    <div class="mb-4 space-y-1.5 w-1/2">
+                        <label for="email"
+                            class="font-sans  text-sm text-slate-800 dark:text-white font-bold mb-2">Nomor
+                            Agenda</label>
+                        <div class="relative w-full">
+                            <input placeholder="Masukkan Nomor Agenda" type="text"
+                                class="w-full aria-disabled:cursor-not-allowed outline-none focus:outline-none text-slate-800 dark:text-white placeholder:text-slate-600/60 bg-transparent ring-transparent border border-slate-200 transition-all duration-300 ease-in disabled:opacity-50 disabled:pointer-events-none data-[error=true]:border-error data-[success=true]:border-success select-none data-[shape=pill]:rounded-full text-sm rounded-md py-2 px-2.5 ring shadow-sm data-[icon-placement=start]:ps-9 data-[icon-placement=end]:pe-9 hover:border-slate-800 hover:ring-slate-800/10 focus:border-slate-800 focus:ring-slate-800/10 peer" />
+                        </div>
+                    </div>
+                    <div class="mb-4 space-y-1.5 w-1/3">
+                        <label for="email"
+                            class="font-sans  text-sm text-slate-800 dark:text-white font-bold mb-2">Tanggal
+                            Terima</label>
+                        <div class="relative w-full">
+                            <input placeholder="Masukkan Nomor Agenda" type="text"
+                                class="w-full aria-disabled:cursor-not-allowed outline-none focus:outline-none text-slate-800 dark:text-white placeholder:text-slate-600/60 bg-transparent ring-transparent border border-slate-200 transition-all duration-300 ease-in disabled:opacity-50 disabled:pointer-events-none data-[error=true]:border-error data-[success=true]:border-success select-none data-[shape=pill]:rounded-full text-sm rounded-md py-2 px-2.5 ring shadow-sm data-[icon-placement=start]:ps-9 data-[icon-placement=end]:pe-9 hover:border-slate-800 hover:ring-slate-800/10 focus:border-slate-800 focus:ring-slate-800/10 peer" />
+                        </div>
+                    </div>
+                    <div class="mb-4 space-y-1.5 w-1/3">
+                        <label for="email"
+                            class="font-sans  text-sm text-slate-800 dark:text-white font-bold mb-2">Tanggal
+                            Surat</label>
+                        <div class="relative w-full">
+                            <input placeholder="Masukkan Nomor Agenda" type="text"
+                                class="w-full aria-disabled:cursor-not-allowed outline-none focus:outline-none text-slate-800 dark:text-white placeholder:text-slate-600/60 bg-transparent ring-transparent border border-slate-200 transition-all duration-300 ease-in disabled:opacity-50 disabled:pointer-events-none data-[error=true]:border-error data-[success=true]:border-success select-none data-[shape=pill]:rounded-full text-sm rounded-md py-2 px-2.5 ring shadow-sm data-[icon-placement=start]:ps-9 data-[icon-placement=end]:pe-9 hover:border-slate-800 hover:ring-slate-800/10 focus:border-slate-800 focus:ring-slate-800/10 peer" />
+                        </div>
                     </div>
                 </div>
-                <div class="mb-4 mt-2 space-y-1.5">
-                    <label for="password"
-                        class="font-sans  text-sm text-slate-800 dark:text-white font-bold mb-2">Password</label>
-                    <div class="relative w-full">
-                        <input placeholder="*******" type="password"
-                            class="w-full aria-disabled:cursor-not-allowed outline-none focus:outline-none text-slate-800 dark:text-white placeholder:text-slate-600/60 bg-transparent ring-transparent border border-slate-200 transition-all duration-300 ease-in disabled:opacity-50 disabled:pointer-events-none data-[error=true]:border-error data-[success=true]:border-success select-none data-[shape=pill]:rounded-full text-sm rounded-md py-2 px-2.5 ring shadow-sm data-[icon-placement=start]:ps-9 data-[icon-placement=end]:pe-9 hover:border-slate-800 hover:ring-slate-800/10 focus:border-slate-800 focus:ring-slate-800/10 peer" />
+                <div class="flex flex-row gap-3 items-center">
+                    <div class="mb-4 space-y-1.5 w-1/2">
+                        <label for="email"
+                            class="font-sans  text-sm text-slate-800 dark:text-white font-bold mb-2">Instansi
+                            Pengirim</label>
+                        <div class="relative w-full">
+                            <input placeholder="Masukkan Instansi Pengirim" type="text"
+                                class="w-full aria-disabled:cursor-not-allowed outline-none focus:outline-none text-slate-800 dark:text-white placeholder:text-slate-600/60 bg-transparent ring-transparent border border-slate-200 transition-all duration-300 ease-in disabled:opacity-50 disabled:pointer-events-none data-[error=true]:border-error data-[success=true]:border-success select-none data-[shape=pill]:rounded-full text-sm rounded-md py-2 px-2.5 ring shadow-sm data-[icon-placement=start]:ps-9 data-[icon-placement=end]:pe-9 hover:border-slate-800 hover:ring-slate-800/10 focus:border-slate-800 focus:ring-slate-800/10 peer" />
+                        </div>
                     </div>
-                </div>
-                <div class="flex items-center gap-2">
-                    <label class="flex items-center cursor-pointer relative" for="checkbox-with-label">
-                        <input type="checkbox" checked
-                            class="peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow-sm  border border-slate-200 checked:bg-slate-800 checked:border-slate-800"
-                            id="checkbox-with-label" />
-                        <span
-                            class="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                            <svg fill="none" width="18px" height="18px" stroke-width="2" color="currentColor"
-                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M5 13L9 17L19 7" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round"></path>
-                            </svg>
-                        </span>
-                    </label>
-                    <label class="cursor-pointer text-slate-600 antialiased" for="checkbox-with-label">Remember
-                        Me</label>
+                    <div class="mb-4 space-y-1.5 w-1/3">
+                        @include('components.collapse', ['label' => 'Klasifikasi'])
+                    </div>
+                    <div class="mb-4 space-y-1.5 w-1/3">
+                        @include('components.collapse', ['label' => 'Sifat'])
+                    </div>
                 </div>
             </div>
-            <div class=" px-4 pb-4 flex flex-col justify-end gap-2">
-                <button type="button"
-                    class="inline-flex items-center justify-center border align-middle select-none font-sans font-medium text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed data-[shape=pill]:rounded-full data-[width=full]:w-full focus:shadow-none text-sm rounded-md py-2 px-4 shadow-sm hover:shadow-md bg-slate-800 border-slate-800 text-slate-50 hover:bg-slate-700 hover:border-slate-700">Sign
-                    In </button>
+            <div class=" px-4 pb-4 flex  justify-end gap-2">
+                @include('components.tombol-simpan-surat')
             </div>
         </div>
     </div>
