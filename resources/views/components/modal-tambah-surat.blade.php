cd6fb2aa7739824a89bd3b8cdf19082681c4e2c1
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-04-16 09:24:15 +0700

    Create textarea components

--- resources/views/components/modal-tambah-surat.blade.php
+++ resources/views/components/modal-tambah-surat.blade.php
@@ -68,11 +68,10 @@
                     </div>
                 </div>
                 <div class="space-y-1.5">
-                    <label for="email"
-                        class="font-sans  text-sm text-slate-800 dark:text-white font-bold mb-2">Perihal Surat</label>
-                    <textarea rows="8" placeholder="Perihal Surat"
-                        class="peer block w-full resize-none rounded-md border border-slate-200 bg-transparent p-2.5 text-sm leading-none text-slate-800 outline-none ring ring-transparent transition-all duration-300 ease-in placeholder:text-slate-600/60 hover:border-slate-800 hover:ring-slate-800/10 focus:border-slate-800 focus:outline-none focus:ring-slate-800/10 disabled:pointer-events-none disabled:opacity-50 dark:text-white"></textarea>
-
+                    @include('components.textarea-surat', [
+                        'label' => 'Perihal',
+                        'placeholder' => 'Masukkan Perihal Surat',
+                    ])
                 </div>
             </div>
             <div class=" px-4 pb-4 flex  justify-end gap-2">
