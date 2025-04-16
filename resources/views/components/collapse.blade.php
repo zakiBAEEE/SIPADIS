0d4860b541c9e23eb350981f414e4dba89d11d2c
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-04-16 08:16:17 +0700

    Build Modal

--- resources/views/components/collapse.blade.php
+++ resources/views/components/collapse.blade.php
@@ -1,13 +1,17 @@
-<div class=" space-y-1.5 w-1/2">
-    <label for="email" class="font-sans  text-sm text-slate-800 dark:text-white font-bold mb-2">Klasifikasi</label>
+<div class="space-y-1.5 w-1/2">
+    <label for="sifat"
+        class="font-sans text-sm text-slate-800 dark:text-white font-bold mb-2">{{ $label }}</label>
     <div class="w-full max-w-sm min-w-[200px]">
         <div class="relative">
-            <select
+            <select name="sifat"
                 class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 ring ring-transparent hover:ring-slate-800/10 focus:ring-slate-800/10 hover:border-slate-800 shadow-sm focus:shadow cursor-pointer appearance-none">
-                <option value="1">Umum</option>
-                <option value="2">Pengaduan</option>
-                <option value="3">Permintaan Informasi</option>
+
+                @foreach ($value as $item)
+                    <option value="{{ $item }}">{{ $item }}</option>
+                @endforeach
+
             </select>
+
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2"
                 stroke="currentColor" class="h-5 w-5 ml-1 absolute top-2.5 right-2.5 text-slate-700">
                 <path stroke-linecap="round" stroke-linejoin="round"
--- resources/views/components/modal-tambah-surat.blade.php
+++ resources/views/components/modal-tambah-surat.blade.php
@@ -65,14 +65,22 @@
                         </div>
                     </div>
                     <div class="mb-4 space-y-1.5 w-1/3">
-                        @include('components.collapse', ['label' => 'Klasifikasi'])
+                        @include('components.collapse', [
+                            'label' => 'Klasifikasi',
+                            'value' => ['Umum', 'Pengaduan', 'Permintaan Informasi'],
+                        ])
                     </div>
                     <div class="mb-4 space-y-1.5 w-1/3">
-                        @include('components.collapse', ['label' => 'Sifat'])
+                        @include('components.collapse', [
+                            'label' => 'Sifat',
+                            'value' => ['Rahasia', 'Penting', 'Segera', 'Rutin'],
+                        ])
                     </div>
                 </div>
-                <div>
-                    <textarea rows="8" placeholder="Perihal Surat Surat"
+                <div class="space-y-1.5">
+                    <label for="email"
+                        class="font-sans  text-sm text-slate-800 dark:text-white font-bold mb-2">Perihal Surat</label>
+                    <textarea rows="8" placeholder="Perihal Surat"
                         class="peer block w-full resize-none rounded-md border border-slate-200 bg-transparent p-2.5 text-sm leading-none text-slate-800 outline-none ring ring-transparent transition-all duration-300 ease-in placeholder:text-slate-600/60 hover:border-slate-800 hover:ring-slate-800/10 focus:border-slate-800 focus:outline-none focus:ring-slate-800/10 disabled:pointer-events-none disabled:opacity-50 dark:text-white"></textarea>
 
                 </div>
