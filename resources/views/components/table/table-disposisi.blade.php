6bf072d51751d877b64a669f83c1897d11fbaa76
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-04-18 23:02:26 +0700

    Memperbaiki tabel disposisi

--- resources/views/components/table/table-disposisi.blade.php
+++ resources/views/components/table/table-disposisi.blade.php
@@ -2,131 +2,47 @@
     <table class="w-full">
         <thead class="border-b border-slate-200 bg-slate-100 text-sm font-medium text-slate-600 dark:bg-slate-900">
             <tr>
-                <th class="px-2.5 py-2 text-start font-medium">
-                    Disposisi
+                <th class="px-2.5 py-2 text-start font-bold">
+                    Tanggal
                 </th>
-                <th class="px-2.5 py-2 text-start font-medium">
-                    Tgl
+                <th class="px-2.5 py-2 text-start font-bold">
+                    Disposisi Dari
                 </th>
-                <th class="px-2.5 py-2 text-start font-medium">
-                    Tujuan Disposisi
-                </th>
-                <th class="px-2.5 py-2 text-start font-medium">
+                <th class="px-2.5 py-2 text-start font-bold">
                     Instruksi
                 </th>
-                <th class="px-2.5 py-2 text-start font-medium">
-                    Paraf
-                </th>
-                <th class="px-2.5 py-2 text-start font-medium">
-                    Tgl
-                </th>
-                <th class="px-2.5 py-2 text-start font-medium">
+                <th class="px-2.5 py-2 text-start font-bold">
                     Tujuan Disposisi
-                </th>
-                <th class="px-2.5 py-2 text-start font-medium">
-                    Instruksi
-                </th>
-                <th class="px-2.5 py-2 text-start font-medium">
-                    Paraf
-                </th>
-                <th class="px-2.5 py-2 text-start font-medium">
-                    Tgl
-                </th>
-                <th class="px-2.5 py-2 text-start font-medium">
-                    Tujuan Disposisi
-                </th>
-                <th class="px-2.5 py-2 text-start font-medium">
-                    Instruksi
-                </th>
-                <th class="px-2.5 py-2 text-start font-medium">
-                    Paraf
                 </th>
             </tr>
         </thead>
         <tbody class="group text-sm text-slate-800 dark:text-white">
             <tr class="even:bg-slate-100 dark:even:bg-slate-900">
                 <td class="p-3">
-                    John Michael
+                    3 Desember 2023
                 </td>
                 <td class="p-3">
-                    Manager
+                    Kepala LLDIKTI
                 </td>
                 <td class="p-3">
-                    23/04/18
+                    Tindaklanjuti surat ini
                 </td>
                 <td class="p-3">
-                    <a href="#"
-                        class="font-sans antialiased text-sm text-slate-800 font-medium hover:text-slate-600">
-                        Edit
-                    </a>
+                    KBU
                 </td>
             </tr>
             <tr class="even:bg-slate-100 dark:even:bg-slate-900">
                 <td class="p-3">
-                    Alexa Liras
+                    4 Desember 2023
                 </td>
                 <td class="p-3">
-                    Developer
+                    KBU
                 </td>
                 <td class="p-3">
-                    23/04/18
+                    Tugaskan Staf untuk Mewakili
                 </td>
                 <td class="p-3">
-                    <a href="#"
-                        class="font-sans antialiased text-sm text-slate-800 font-medium hover:text-slate-600">
-                        Edit
-                    </a>
-                </td>
-            </tr>
-            <tr class="even:bg-slate-100 dark:even:bg-slate-900">
-                <td class="p-3">
-                    Laurent Perrier
-                </td>
-                <td class="p-3">
-                    Executive
-                </td>
-                <td class="p-3">
-                    19/09/17
-                </td>
-                <td class="p-3">
-                    <a href="#"
-                        class="font-sans antialiased text-sm text-slate-800 font-medium hover:text-slate-600">
-                        Edit
-                    </a>
-                </td>
-            </tr>
-            <tr class="even:bg-slate-100 dark:even:bg-slate-900">
-                <td class="p-3">
-                    Michael Levi
-                </td>
-                <td class="p-3">
-                    Developer
-                </td>
-                <td class="p-3">
-                    24/12/08
-                </td>
-                <td class="p-3">
-                    <a href="#"
-                        class="font-sans antialiased text-sm text-slate-800 font-medium hover:text-slate-600">
-                        Edit
-                    </a>
-                </td>
-            </tr>
-            <tr class="even:bg-slate-100 dark:even:bg-slate-900">
-                <td class="p-3">
-                    Richard Gran
-                </td>
-                <td class="p-3">
-                    Manager
-                </td>
-                <td class="p-3">
-                    04/10/21
-                </td>
-                <td class="p-3">
-                    <a href="#"
-                        class="font-sans antialiased text-sm text-slate-800 font-medium hover:text-slate-600">
-                        Edit
-                    </a>
+                    Belmawa
                 </td>
             </tr>
         </tbody>
