c2949363a4802026535cdd32411b23d7100b931a
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-04-18 17:50:21 +0700

    Create detail surat pages

---
+++ resources/views/pages/super-admin/detail-surat.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.super-admin-layout')
+
+
+@section('content')
+    <div class="bg-white w-full h-full rounded-xl shadow-neutral-400 shadow-lg overflow-scroll p-4">
+
+    </div>
+@endsection
