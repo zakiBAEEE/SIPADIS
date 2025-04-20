6a0ada87f1385e15604f4b38418d860c951eb3e3
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-04-20 14:16:15 +0700

    Create tombol filter

--- resources/views/components/form/form-surat-masuk.blade.php
+++ resources/views/components/form/form-surat-masuk.blade.php
@@ -1,4 +1,4 @@
- <div class="p-4">
+ <div class="px-4 py-2">
      <div class="flex flex-row gap-3">
          <div class="mb-4 space-y-1.5 w-1/2">
              @include('components.base.input-surat', [
--- resources/views/components/layout/collapse-filter.blade.php
+++ resources/views/components/layout/collapse-filter.blade.php
@@ -1,3 +1,13 @@
 <div class="flex-col transition-[max-height] duration-300 ease-in-out max-h-0 mt-1" id = "collapseFilterSurat">
     @include('components.form.form-surat-masuk')
+    <div class="flex flex-row justify-end mb-5 gap-4">
+        <button
+            class="inline-flex border font-sans font-medium text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed data-[shape=pill]:rounded-full data-[width=full]:w-full focus:shadow-none text-sm rounded-md py-1 px-2 shadow-sm hover:shadow bg-red-800 border-red-800 text-slate-50 hover:bg-slate-700 hover:border-slate-700">
+            Reset
+        </button>
+        <button
+            class="inline-flex border font-sans font-medium text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed data-[shape=pill]:rounded-full data-[width=full]:w-full focus:shadow-none text-sm rounded-md py-1 px-2 shadow-sm hover:shadow bg-slate-800 border-slate-800 text-slate-50 hover:bg-slate-700 hover:border-slate-700">
+            Terapkan
+        </button>
+    </div>
 </div>
