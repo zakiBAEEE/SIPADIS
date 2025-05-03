29b33ba6e78b62f2a92566491640e8dbd6668bdd
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-05-03 07:36:28 +0700

    Menghapus komentar

--- resources/views/components/layout/card-dashboard.blade.php
+++ resources/views/components/layout/card-dashboard.blade.php
@@ -3,7 +3,6 @@
 <div
     class="w-full max-w-xs overflow-hidden rounded-lg border border-slate-200 bg-white shadow-lg shadow-slate-950/5 flex justify-center items-center">
     <div class="h-max w-full rounded px-3 py-2 flex flex-col items-center justify-center gap-2">
-        {{-- pilih icon berdasarkan $jenis --}}
         @if ($jenis === 'total')
             @include('components.base.ikon-surat')
         @elseif($jenis === 'umum')
@@ -13,11 +12,9 @@
         @elseif($jenis === 'permintaan informasi')
             @include('components.base.ikon-surat-permintaan-informasi')
         @else
-            {{-- fallback icon --}}
             @include('components.base.ikon-surat')
         @endif
 
-        {{-- judul dinamis --}}
         <h6 class="font-sans text-base font-bold antialiased md:text-lg lg:text-xl text-gray-700 text-center">
             @if ($jenis === 'total')
                 {{ $count ?? 0 }} Surat {{ ucfirst($jenis) }}
