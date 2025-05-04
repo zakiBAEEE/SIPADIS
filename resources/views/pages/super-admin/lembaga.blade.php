b112f449d7094dea0193ba561f667f9f099eacae
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-05-05 02:13:15 +0700

    Layouting tombol Edit surat

--- resources/views/pages/super-admin/lembaga.blade.php
+++ resources/views/pages/super-admin/lembaga.blade.php
@@ -4,19 +4,22 @@
     <div
         class="bg-white min-w-full h-full rounded-xl shadow-neutral-400 shadow-lg overflow-scroll p-4 flex flex-col gap-y-6">
         <div class="flex flex-col p-4">
-            <div class="flex flex-row justify-between">
+            <div class="flex flex-row justify-between items-end">
                 <div>
                     <h4 class="font-sans text-xl font-bold antialiased md:text-2xl lg:text-2xl text-gray-600">Data Lembaga
                     </h4>
-                    <h6 class="font-sans text-base font-bold antialiased md:text-lg lg:text-lg text-gray-600">LLDIKTI Wilayah 2
+                    <h6 class="font-sans text-base font-bold antialiased md:text-lg lg:text-lg text-gray-600">LLDIKTI Wilayah
+                        2
                     </h6>
-                    <hr class="w-full border-t border-gray-300 my-1" />
                 </div>
-                <a href="{{ route('lembaga.edit') }}"
-                    class="flex border font-medium font-sans text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed data-[shape=pill]:rounded-full data-[width=full]:w-full focus:shadow-none text-sm rounded-md px-1 shadow-sm hover:shadow-md  border-orange-400 text-slate-800  bg-orange-300 flex-row justify-center items-center gap-1 cursor-pointer">
-                    @include('components.base.ikon-edit') Edit
-                </a>
+                <div class="h-[30px]">
+                    <a href="{{ route('lembaga.edit') }}"
+                        class="flex border font-medium font-sans text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed data-[shape=pill]:rounded-full data-[width=full]:w-full focus:shadow-none text-sm rounded-md px-1 shadow-sm hover:shadow-md  border-orange-400 text-slate-800  bg-orange-300 flex-row justify-center items-center gap-1 cursor-pointer h-full">
+                        @include('components.base.ikon-edit') Edit
+                    </a>
+                </div>
             </div>
+            <hr class="w-full border-t border-gray-300 my-1" />
             <div class="flex flex-col gap-5">
                 <div class="flex flex-col">
                     <div class="flex flex-row gap-x-5">
