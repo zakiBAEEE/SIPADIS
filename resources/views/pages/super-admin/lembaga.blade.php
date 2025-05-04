f423eab21905069d2d3025b7badddf2fb3251814
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-05-05 00:25:43 +0700

    Layouting Lembaga Pages

--- resources/views/pages/super-admin/lembaga.blade.php
+++ resources/views/pages/super-admin/lembaga.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div
         class="bg-white min-w-full h-full rounded-xl shadow-neutral-400 shadow-lg overflow-scroll p-4 flex flex-col gap-y-6">
-        <div class="flex flex-col">
+        <div class="flex flex-col p-4">
             <div>
                 <h4 class="font-sans text-xl font-bold antialiased md:text-2xl lg:text-2xl text-gray-600">Data Lembaga
                 </h4>
@@ -12,7 +12,7 @@
                 <hr class="w-full border-t border-gray-300 my-1" />
             </div>
             <div class="flex flex-col">
-                <div class="flex flex-row gap-x-5 p-4">
+                <div class="flex flex-row gap-x-5">
                     <div class="flex flex-col w-5/6 gap-3">
                         @include('components.base.input-surat', [
                             'label' => 'Nama Kementrian',
@@ -56,7 +56,54 @@
                     </div>
                 </div>
             </div>
-
+            <div class="flex flex-col gap-3">
+                <div class="flex flex-row gap-3 w-full">
+                    <div class="w-1/3">
+                        @include('components.base.input-surat', [
+                            'label' => 'Tahun',
+                            'placeholder' => 'Masukkan Tahun',
+                            'name' => 'tahun',
+                        ])
+                    </div>
+                    <div class="w-1/3">
+                        @include('components.base.input-surat', [
+                            'label' => 'Kota',
+                            'placeholder' => 'Masukkan Nama Kota',
+                            'name' => 'kota',
+                        ])
+                    </div>
+                    <div class="w-1/3">
+                        @include('components.base.input-surat', [
+                            'label' => 'Provinsi',
+                            'placeholder' => 'Masukkan Nama Provinsi',
+                            'name' => 'provinsi',
+                        ])
+                    </div>
+                </div>
+                <div class="flex flex-row gap-3">
+                    <div class="w-1/3">
+                        @include('components.base.input-surat', [
+                            'label' => 'Nama Kementrian',
+                            'placeholder' => 'Masukkan Nama Kementrian',
+                            'name' => 'nama_kementrian',
+                        ])
+                    </div>
+                    <div class="w-1/3">
+                        @include('components.base.input-surat', [
+                            'label' => 'Kepala Kantor',
+                            'placeholder' => 'Masukkan Nama Kepala Kantor',
+                            'name' => 'kepala_kantor',
+                        ])
+                    </div>
+                    <div class="w-1/3">
+                        @include('components.base.input-surat', [
+                            'label' => 'NIP',
+                            'placeholder' => 'Masukkan NIP Kepala Kantor',
+                            'name' => 'nip_kepala_kantor',
+                        ])
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
