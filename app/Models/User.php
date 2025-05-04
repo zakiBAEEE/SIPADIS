91071756e2dd95ff69791d34469a500e9f47eb29
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-05-04 07:21:01 +0700

    Make model users

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,55 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
-        'email',
+        'username',
         'password',
+        'role_id',
+        'divisi_id',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // Relasi ke Divisi
+    public function divisi()
+    {
+        return $this->belongsTo(Divisi::class);
+    }
+
+    // Relasi ke Role (jika kamu punya model Role)
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    // Disposisi yang dikirim user ini
+    public function disposisiDikirim()
+    {
+        return $this->hasMany(Disposisi::class, 'dari_user_id');
+    }
+
+    // Disposisi yang diterima user ini
+    public function disposisiDiterima()
+    {
+        return $this->hasMany(Disposisi::class, 'ke_user_id');
     }
 }
