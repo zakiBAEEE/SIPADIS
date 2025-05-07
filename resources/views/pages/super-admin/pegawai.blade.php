50a9828c1cc22d8f6e9d9ad6a2f3731e4226c017
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-05-07 20:05:19 +0700

    Initialize Pegawai Page

--- resources/views/pages/super-admin/pegawai.blade.php
+++ resources/views/pages/super-admin/pegawai.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.super-admin-layout')
 
 @section('content')
-    <div>
-
+    <div class="bg-white w-full h-full rounded-xl shadow-neutral-400 shadow-lg overflow-scroll p-4">
+        <div class="flex flex-row justify-between items-center w-full">
+            <div>
+                <h4 class="font-sans text-xl font-bold antialiased md:text-2xl lg:text-2xl text-gray-600">Data Pegawai
+                </h4>
+                <h6 class="font-sans text-base font-bold antialiased md:text-lg lg:text-lg text-gray-600">LLDIKTI Wilayah 2
+                </h6>
+            </div>
+            <div class="flex flex-row gap-2">
+                @include('components.layout.modal-tambah-surat')
+            </div>
+        </div>
+        <hr class="w-full border-t border-gray-300 my-4" />
+        {{-- @include('components.table.table', ['surats' => $surats])
+        <div class="mt-4 flex flex-row justify-end">
+            @include('components.base.pagination', ['surats' => $surats])
+        </div> --}}
     </div>
 @endsection
