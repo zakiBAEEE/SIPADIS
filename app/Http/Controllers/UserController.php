5d18ee15c61e3f73dae5e3896901e7e76bbeec39
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-05-08 09:38:26 +0700

    Initialize pegawai table

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
     public function index()
     {
         // Mengambil semua user dari database
-        $users = User::all();
+         $users = User::orderBy('created_at', 'desc')->paginate(8); // Menggunakan pagination untuk menampilkan 10 user per halaman
 
         // Mengirimkan data users ke view 'users.index'
         return view('pages.super-admin.pegawai', compact('users'));
