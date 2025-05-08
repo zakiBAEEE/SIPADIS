3d5089bb296f3af4ffa77b0d4c0603f92c9ad1f1
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-05-08 09:51:14 +0700

    Create controller TimKerja

--- app/Http/Controllers/TimKerjaController.php
+++ app/Http/Controllers/TimKerjaController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Divisi; // Pastikan mengimpor model User
 use Illuminate\Http\Request;
 
 class TimKerjaController extends Controller
 {
-    //
+        public function index()
+    {
+        // $surats = SuratMasuk::orderBy('created_at', 'desc')->get();;
+        $timKerja = Divisi::orderBy('created_at', 'desc')->paginate(8); 
+        return view('pages.super-admin.tim-kerja', compact('timKerja'));
+    }
 }
