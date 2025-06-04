bcb49c5fb6c7bf195455d228e73cb96808f25fc4
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-06-04 12:21:29 +0700

    Minor Changes

--- resources/views/components/table/table-agenda-kepala.blade.php
+++ resources/views/components/table/table-agenda-kepala.blade.php
@@ -1,5 +1,6 @@
-<table class="w-full table-fixed border border-black border-collapse text-[9px]">
-    <thead class="bg-gray-200">
+<div class="relative flex flex-col w-full max-h-[400px] overflow-scroll text-gray-700 bg-white shadow-md rounded-lg bg-clip-border h-[400px]">
+<table class="w-full text-left table-auto text-slate-800 min-w-0">
+    <thead class="bg-slate-50">
         <tr>
             @php
                 $headers = [
@@ -15,7 +16,7 @@
                 ];
             @endphp
             @foreach ($headers as $header)
-                <th class="border border-black px-1 py-1 break-words whitespace-normal text-wrap">
+                <th class="border border-slate-300 px-3 py-2 text-left text-slate-500 bg-slate-100">
                     {{ $header }}
                 </th>
             @endforeach
@@ -24,27 +25,27 @@
     <tbody>
         @foreach ($suratMasuk as $surat)
             @foreach ($surat->disposisis as $disposisi)
-                <tr class="break-inside-avoid">
-                    <td class="border border-black px-1 py-1 text-center break-words whitespace-normal">
+                <tr class="hover:bg-slate-50">
+                    <td class="border border-slate-300 px-3 py-2 text-center text-slate-800">
                         {{ $surat->nomor_agenda }}
                     </td>
-                    <td class="border border-black px-1 py-1 break-words whitespace-normal">
+                    <td class="border border-slate-300 px-3 py-2 text-slate-800">
                         {{ \Carbon\Carbon::parse($surat->tanggal_terima)->translatedFormat('d M Y') }}
                     </td>
-                    <td class="border border-black px-1 py-1 break-words whitespace-normal">
+                    <td class="border border-slate-300 px-3 py-2 text-slate-800">
                         {{ $surat->pengirim }}
                     </td>
-                    <td class="border border-black px-1 py-1 break-words whitespace-normal">
+                    <td class="border border-slate-300 px-3 py-2 text-slate-800">
                         {{ \Carbon\Carbon::parse($surat->tanggal_surat)->translatedFormat('d M Y') }}
                     </td>
-                    <td class="border border-black px-1 py-1 break-words whitespace-normal">
+                    <td class="border border-slate-300 px-3 py-2 text-slate-800">
                         {{ $surat->nomor_surat }}
                     </td>
-                    <td class="border border-black px-1 py-1 break-words whitespace-normal">
+                    <td class="border border-slate-300 px-3 py-2 text-slate-800">
                         {{ $surat->perihal }}
                     </td>
 
-                    <td class="border border-black px-1 py-1 break-words whitespace-normal">
+                    <td class="border border-slate-300 px-3 py-2 text-slate-800">
                         @if ($disposisi->penerima)
                             @php
                                 $penerima = $disposisi->penerima;
@@ -56,14 +57,17 @@
                         @endif
                     </td>
 
-                    <td class="border border-black px-1 py-1 break-words whitespace-normal">
+                    <td class="border border-slate-300 px-3 py-2 text-slate-800">
                         {{ $disposisi->catatan ?? '-' }}
                     </td>
 
-                    <td class="border border-black px-1 py-1 break-words whitespace-normal text-center">
+                    <td class="border border-slate-300 px-3 py-2 text-center text-slate-800">
                     </td>
                 </tr>
             @endforeach
         @endforeach
     </tbody>
 </table>
+</div>
+
+
