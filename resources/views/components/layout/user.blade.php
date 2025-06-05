cacd81367f16bba9146f281703dd04ab0f7d4977
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-06-05 11:17:52 +0700

    Autentikasi

--- resources/views/components/layout/user.blade.php
+++ resources/views/components/layout/user.blade.php
@@ -1,10 +1,12 @@
+@auth {{-- Memastikan blok ini hanya ditampilkan jika ada pengguna yang login --}}
 <div class="flex flex-row w-full mt-4 ml-3">
-    <img class="inline-block h-11 w-11 rounded-full object-cover object-center"
-        src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/ct-assets/team-4.jpg"
-        alt="profile-picture" />
-
     <div class="ml-3 flex flex-col">
-        <span class="text-md font-semibold text-gray-900">John Doe | 2226250100</span>
-        <span class="text-sm font-normal text-gray-500">Super Admin</span>
+        <span class="text-md font-semibold text-gray-900">
+            {{ Auth::user()->name }} | {{ Auth::user()->username }}
+        </span>
+        <span class="text-sm font-normal text-gray-500">
+            {{ Auth::user()->role->name }} 
+        </span>
     </div>
 </div>
+@endauth
