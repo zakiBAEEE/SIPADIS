906625066447e576e4deec12e8e7efaed2fe668e
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-07-02 10:53:49 +0700

    Add Reset Handler

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,11 +5,14 @@ import { initMaterialTailwind } from '@material-tailwind/html';
 import Alpine from 'alpinejs';
 import { initializeCharts } from './components/initChart';
 import { initializeAlerts } from './components/alert-handler';
+import { handleFormReset } from './components/resetForm';
+
 
 window.Alpine = Alpine;
 Alpine.start();
 
 document.addEventListener('DOMContentLoaded', () => {
+    handleFormReset('#filterSuratKlasifikasi form', '#filterSuratKlasifikasi button[type="reset"]');
     initializeAlerts();
     initializeCharts();
     initMaterialTailwind();
